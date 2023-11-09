8f0af8f0f7d26e339374e542efe5fdac51e0e988
Rigo-rios <146138438+Rigo-rios@users.noreply.github.com>
2023-11-08 21:28:49 -0500

    First Commit

---
+++ StreamingContentData/StreamingContentEntity.cs
@@ -0,0 +1,16 @@
+namespace StreamingContentData;
+
+// Applicatoin -- User can keep an up to date list of streaming content
+// There is several different kinds of streaming content - movies, tv shows, etc.
+// Collection of streaming content
+// Repository pattern architecture - data and business logic seperate
+
+// Actors, durations, and rating are different properties. 
+// Interfaces are actions that we can perform (peel function)
+
+
+// Inheritance With Streaming Content Objects
+public class StreamingContentEntity
+{
+    
+}
